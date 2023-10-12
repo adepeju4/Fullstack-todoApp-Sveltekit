f7f75cda29da336b8665b431e4f26757d1d15641
Adepeju <adepeju.kayode@gmail>
2023-10-12 15:35:15 +0100

    added sveltekit adapter for vercel

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,5 +1,9 @@
 /** @type {import('./$types').PageLoad} */
 
+import type { Config } from '@sveltejs/adapter-vercel';
+
+export const config: Config = { runtime: 'nodejs18.x' };
+
 export const load = ({ locals }: { locals: App.Locals }) => {
 	if (locals?.user) {
 		return {
